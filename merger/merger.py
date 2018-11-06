1512e50f9408a56f19446bb13aa9afa95d1b363c
dkati <dkatikaridis@gmail.com>
2018-11-06 12:17:24 +0200

    merger: Read the files

--- merger/merger.py
+++ merger/merger.py
@@ -6,3 +6,9 @@ mergedFilename	="OUT/merged.csv";
 if os.path.exists(mergedFilename):
 	print('Old indexes file deleted');
 	os.remove(mergedFilename);
+	
+with open(productFilename,"r") as hProduct:
+	productLines = hProduct.readlines();
+with open(facetsFilename,"r") as hFacets:
+	facetsLines = hFacets.readlines();
+	
