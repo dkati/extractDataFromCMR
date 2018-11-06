eeebd6b14f50976f980c58a360ea62a9fadcb8d5
dkati <dkatikaridis@gmail.com>
2018-11-06 12:51:50 +0200

    merger: Fix up the CSV creation of the product

--- merger/merger.py
+++ merger/merger.py
@@ -11,20 +11,25 @@ if os.path.exists(productCSVFilename):
 	print('Old product CSV file deleted');
 	os.remove(productCSVFilename);
 	
+hCSVproduct = open(productCSVFilename,"a");
 with open(productFilename,"r") as hProduct:
 	productLines = hProduct.readlines();
+	index=0;
+	#clean out the '[' and ']'...
+	for line in productLines:
+		productLines[index] = productLines[index].replace("[","");
+		productLines[index] = productLines[index].replace("]","");
+		index+=1;
+	#Convert to CSV and paste it to OUT folder
+	for line in productLines:
+		hCSVproduct.write(line);  
 with open(facetsFilename,"r") as hFacets:
 	facetsLines = hFacets.readlines();
+hCSVproduct.close(); #Close the stream
 
-#clean out the '[' and ']', ...
-index=0;
-for line in productLines:
-	productLines[index] = productLines[index].replace("[","");
-	productLines[index] = productLines[index].replace("]","");
-	index+=1;
-#...and convert the file to csv
-with open(productCSVFilename,"a") as hProduct:
-	for line in productLines:
-		hProduct.write(line);
-
+# Read again the product CSV
+with open(productCSVFilename,"r") as hProduct:
+	productLines = hProduct.readlines();
 	
+validIDs=[];
+for line in productLines:
