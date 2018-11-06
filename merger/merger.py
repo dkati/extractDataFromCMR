efe9d2c433dc9fd73c87ab68dddd6dae2631819a
dkati <dkatikaridis@gmail.com>
2018-11-06 12:41:38 +0200

    merger: product production: Remove '[' and ']' , convert CSV

--- merger/merger.py
+++ merger/merger.py
@@ -3,12 +3,28 @@ from os.path import isfile,join
 productFilename	="_product.txt";
 facetsFilename	="facetsOUT.csv";
 mergedFilename	="OUT/merged.csv";
+productCSVFilename = "OUT/product.csv";
 if os.path.exists(mergedFilename):
 	print('Old indexes file deleted');
 	os.remove(mergedFilename);
+if os.path.exists(productCSVFilename):
+	print('Old product CSV file deleted');
+	os.remove(productCSVFilename);
 	
 with open(productFilename,"r") as hProduct:
 	productLines = hProduct.readlines();
 with open(facetsFilename,"r") as hFacets:
 	facetsLines = hFacets.readlines();
+
+#clean out the '[' and ']', ...
+index=0;
+for line in productLines:
+	productLines[index] = productLines[index].replace("[","");
+	productLines[index] = productLines[index].replace("]","");
+	index+=1;
+#...and convert the file to csv
+with open(productCSVFilename,"a") as hProduct:
+	for line in productLines:
+		hProduct.write(line);
+
 	
